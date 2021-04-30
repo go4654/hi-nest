dda447998d67a4dfd1b694c55231751329007e25
go4654 <go4654@naver.com>
2021-04-30 21:40:08 +0900

    unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,92 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll", () => {
+    it("should retrun an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array)
+    })
+  })
+
+  describe("getOne", () => {
+    it("shold return a movie", () => {
+      service.create({
+        title:"Test Movie",
+        genres:["Ttest"],
+        year:2000
+      })
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    })
+    it("should throw 404 error", () => {
+      try{
+        service.getOne(999)
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`해당 아이디:999 영화를 찾을수없습니다`)
+      }
+    })
+  })
+
+  describe("deleteOne", () => {
+    it("영화삭제", () => {
+      service.create({
+        title:"Test Movie",
+        genres:["Ttest"],
+        year:2000
+      })
+      // console.log(service.getAll())
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1)
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete)
+    })
+    it("404를 리턴함",() => {
+      try{
+        service.deleteOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`해당 아이디:999 영화를 찾을수없습니다`)
+      }
+    })
+  })
+
+  describe("create",() => {
+    it("영화 만들기", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title:"Test Movie",
+        genres:["Ttest"],
+        year:2000
+      })
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate)
+
+    });
+  })
+
+  describe("updata", () => {
+    it("영화 수정", () => {
+      service.create({
+        title:"Test Movie",
+        genres:["Ttest"],
+        year:2000
+      })
+      service.update(1, {title:"update test"});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('update test');
+    })
+
+    it("404를 리턴함",() => {
+      try{
+        service.update(999,{});
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`해당 아이디:999 영화를 찾을수없습니다`)
+      }
+    })
+  })
+
 });
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,27 +11,27 @@ export class MoviesService {
         return this.movies;
     }
 
-    getOne(id:string):Movie {
-        const movie = this.movies.find(movie => movie.id === parseInt(id))
+    getOne(id:number):Movie {
+        const movie = this.movies.find(movie => movie.id === id)
         if(!movie){
             throw new NotFoundException(`해당 아이디:${id} 영화를 찾을수없습니다`);
         }
         return movie
     }
 
-    deleteOne(id:string) {
+    deleteOne(id:number) {
         this.getOne(id)
-        this.movies = this.movies.filter(movie => movie.id !== parseInt(id))
+        this.movies = this.movies.filter(movie => movie.id !== id)
     }
 
-    create(movieData){
+    create(movieData:CreateMovieDto){
         this.movies.push({
             id:this.movies.length + 1,
             ...movieData
         })
     }
 
-    update(id:string, updateData){
+    update(id:number, updateData:UpdateMovieDto){
         const movie = this.getOne(id);
         this.deleteOne(id)
         this.movies.push({...movie, ...updateData})
